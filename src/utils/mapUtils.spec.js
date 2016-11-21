f8b389e9135b60f8fc8a6bc0aba97342eecdecce
Alexander Pirozhkov <funkysolution@gmail.com>
2016-11-21 16:23:21 +0500

    utils test

--- src/utils/mapUtils.spec.js
+++ src/utils/mapUtils.spec.js
@@ -1,7 +1,22 @@
 import { expect } from 'chai';
-import { formatSelectOptions } from './mapUtils';
+import { extractInputData, formatSelectOptions } from './mapUtils';
 
 describe('Map Utils', () => {
+  describe('Extract Input Data', () => {
+    it('should return object with data of input component using it\'s event', () => {
+      const mockEmailInputEvent = {
+        target: {
+          name: 'email',
+          type: 'email',
+          value: '123@example.com'
+        }
+      };
+
+      const actual = extractInputData(mockEmailInputEvent);
+      const expected = {field: 'email', val: '123@example.com'};
+      expect(actual).to.deep.equal(expected);
+    });
+  });
   describe('Format Select Options', () => {
     it('should return array of objects formatted for use in a SelectInput component', () => {
       const options = ['EN', 'FR', 'JP'];
@@ -17,7 +32,6 @@ describe('Map Utils', () => {
         {value: 'FR', text: 'French'},
         {value: 'JP', text: 'Japanese'}
       ];
-
       expect(actual).to.deep.equal(expected);
     });
   });
