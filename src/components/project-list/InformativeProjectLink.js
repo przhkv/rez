5b3451426760bc7f854a7c34392effc31057ec78
Alexander Pirozhkov <funkysolution@gmail.com>
2016-11-29 19:18:27 +0500

    removed unnecessary link from InformativeProjectLink component

--- src/components/project-list/InformativeProjectLink.js
+++ src/components/project-list/InformativeProjectLink.js
@@ -1,35 +1,27 @@
 import React, { PropTypes } from 'react';
 
 const InformativeProjectLink = ({i18n, loadedProjectsIds, openProject, projectListItem, theme}) => {
-  const click = e => {
-    e.preventDefault();
-    const {id, title} = e.currentTarget;
-    openProject(id, title);
-  };
-
+  const click = () => openProject(projectListItem.id, projectListItem.idTitle);
   return (
-    <a
-      className={'link dt w-100 bb mt2 dim ' + theme.sectionBorder}
-      href="#"
-      id={projectListItem.id}
-      title={projectListItem.idTitle}
+    <div
+      className={`link dt w-100 bb mt2 dim pointer ${theme.sectionBorder}`}
       onClick={click}
     >
       <div className="dtc v-top pl2">
-        <h1 className={'f6 f5-ns fw6 lh-title mv0 ' + theme.commonText}>
+        <h1 className={`f6 f5-ns fw6 lh-title mv0 ${theme.commonText}`}>
           {projectListItem.title}
         </h1>
-        <dd className={'f6 lh-title ml0 ' + theme.informativeProjectLinkInfo}>
+        <dd className={`f6 lh-title ml0 ${theme.informativeProjectLinkInfo}`}>
           {i18n.project.created + ': ' + projectListItem.created}
         </dd>
         <dl className="mt0 f6">
           <dt className="clip">{i18n.home.status}</dt>
-          <dd className={'ml0 fw6 ' + theme.informativeProjectLinkStatus}>
+          <dd className={`ml0 fw6 ${theme.informativeProjectLinkStatus}`}>
             {loadedProjectsIds.find(id => id === projectListItem.id) && i18n.controls.opened}
           </dd>
         </dl>
       </div>
-    </a>
+    </div>
   );
 };
 
