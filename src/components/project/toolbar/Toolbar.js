07626b1b2634b684b42a25426a9d77534234c086
Alexander Pirozhkov <funkysolution@gmail.com>
2017-04-22 16:12:38 +0500

    add skeleton for timeline ruler

--- src/components/project/toolbar/Toolbar.js
+++ src/components/project/toolbar/Toolbar.js
@@ -2,10 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { List } from 'immutable';
 import AddRemoveChannel from './controls/AddRemoveChannel';
+import TimelineRuler from './ruler/TimelineRuler';
 
 const Toolbar = props => (
   <div className={`flex-none order-2 w-100 bg-near-white ${props.theme.bg}`}>
     <AddRemoveChannel {...props} />
+    <TimelineRuler />
   </div>
 );
 
---
+++ src/components/project/toolbar/ruler/TimelineRuler.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+class TimelineRuler extends React.Component {
+  componentDidMount() {
+    const c = document.getElementById('timelineScale');
+    c.height = 16;
+    c.width = window.innerWidth - 256;
+    const ctx = c.getContext('2d');
+    ctx.strokeStyle = 'DimGray';
+    for (let i = 0; i < 100; i++) {
+      const x1 = (i * 40) + (i % 2 ? 0.5 : 0.9);
+      ctx.moveTo(x1, 12);
+      ctx.lineTo(x1, 18);
+      ctx.stroke();
+      const x2 = (i * 40) + 20.5;
+      ctx.moveTo(x2, 14);
+      ctx.lineTo(x2, 18);
+      ctx.stroke();
+    }
+  }
+
+  render() {
+    return (
+      <canvas id="timelineScale" />
+    );
+  }
+}
+
+export default TimelineRuler;
