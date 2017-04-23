3213a01b0dd49ed46eb4d97a40da36629ea93742
Alexander Pirozhkov <funkysolution@gmail.com>
2017-04-23 16:38:06 +0500

    draft for channels timeline grid

--- src/components/project/3-channels-grid/timeline/ChannelTimeline.js
+++ src/components/project/3-channels-grid/timeline/ChannelTimeline.js
@@ -1,30 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Map } from 'immutable';
-import { isTrue, reverse } from '../../../../utils/stringBoolUtils';
 
-const magicHeight = expanded => `${expanded ? '75' : '53'}px`;
-
-const ChannelTimeline = ({ channel, editedChannelId, i18n, setMouseOut, setMouseOver, theme,
-                           updateProject }) => {
-  const chId = channel.get('channelId');
-  const {expanded, muted} = channel.getIn(['payload', 'state']).toJS();
-  const edited = (chId === editedChannelId);
-  const height = magicHeight(isTrue(expanded));
-
-  return (
-    <div
-      key={chId}
-      className={`bb b--light-gray ${edited ? theme.seqTimelineSelectedBg : theme.seqTimelineBg}`}
-      style={{ height }}
-    >
-      <span className="ml2 light-gray i">todo timeline</span>
-    </div>
-  );
-};
+const ChannelTimeline = ({ channel, channelHeight, editedChannelId, i18n, setMouseOut,
+                           setMouseOver, theme, updateProject }) =>
+(
+  <div className="bb b--light-gray" style={{ height: channelHeight }}>
+    <span className="ml2 light-gray i">todo timeline</span>
+  </div>
+);
 
 ChannelTimeline.propTypes = {
   channel: PropTypes.instanceOf(Map).isRequired,
+  channelHeight: PropTypes.number.isRequired,
   editedChannelId: PropTypes.string.isRequired,
   i18n: PropTypes.object.isRequired,
   setMouseOut: PropTypes.func.isRequired,
--- src/components/project/3-channels-grid/timeline/Timeline.js
+++ src/components/project/3-channels-grid/timeline/Timeline.js
@@ -2,26 +2,68 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Map } from 'immutable';
 import ChannelTimeline from './ChannelTimeline';
+import { isTrue } from '../../../../utils/stringBoolUtils';
 
-const Timeline = ({i18n, project, setMouseOut, setMouseOver, theme, updateProject}) => (
-  <div className="overflow-x-hidden nowrap">
-    {
-      project.get('channels')
-        .map(c => (
-          <ChannelTimeline
-            key={c.get('channelId')}
-            channel={c}
-            editedChannelId={project.get('editedChannelId')}
-            i18n={i18n}
-            setMouseOut={setMouseOut}
-            setMouseOver={setMouseOver}
-            theme={theme}
-            updateProject={updateProject}
-          />
-        ))
+const chHeight = expanded => (isTrue(expanded) ? 75 : 53);
+
+class Timeline extends React.Component {
+  constructor() {
+    super();
+    this.timelineWidth = 0;
+  }
+
+  componentDidMount() {
+    this.timelineWidth = window.innerWidth - 256; // recalculate on resize
+
+    const c = document.getElementById('channelsTimeline');
+    c.width = this.timelineWidth;
+    const canvasHeight =
+      this.props.project.get('channels')
+        .map(ch => chHeight(ch.getIn(['payload', 'state', 'expanded'])))
+        .reduce(((a, b) => a + b), 0);
+    c.height = canvasHeight;
+    const ctx = c.getContext('2d');
+    ctx.strokeStyle = 'DimGray';
+    for (let i = 0; i < 100; i++) {
+      const x1 = (i * 40) + 0.5;
+      ctx.moveTo(x1, 0);
+      ctx.lineTo(x1, canvasHeight);
+      ctx.stroke();
+      const x2 = (i * 40) + 20.5;
+      ctx.moveTo(x2, 0);
+      ctx.lineTo(x2, canvasHeight);
+      ctx.stroke();
     }
-  </div>
-);
+  }
+
+  render() {
+    const { i18n, project, setMouseOut, setMouseOver, theme, updateProject } = this.props;
+
+    return (
+      <div className="overflow-x-hidden nowrap">
+        <canvas id="channelsTimeline" className="absolute o-20" />
+        <div className="absolute" style={{ width: this.timelineWidth }}>
+          {
+            project.get('channels')
+              .map(c => (
+                <ChannelTimeline
+                  key={c.get('channelId')}
+                  channel={c}
+                  channelHeight={chHeight(c.getIn(['payload', 'state', 'expanded']))}
+                  editedChannelId={project.get('editedChannelId')}
+                  i18n={i18n}
+                  setMouseOut={setMouseOut}
+                  setMouseOver={setMouseOver}
+                  theme={theme}
+                  updateProject={updateProject}
+                />
+              ))
+          }
+        </div>
+      </div>
+    );
+  }
+}
 
 Timeline.propTypes = {
   i18n: PropTypes.object.isRequired,
