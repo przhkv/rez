92dbf51d3c75490ff63800856b70b8898c9d0a3a
Alexander Pirozhkov <funkysolution@gmail.com>
2017-05-03 21:18:11 +0500

    calc height for playback pole

--- src/components/project/3-channels-grid/timeline/Timeline.js
+++ src/components/project/3-channels-grid/timeline/Timeline.js
@@ -8,24 +8,20 @@ import { msToTimeString } from '../../../../utils/sequencer/timeFormat';
 
 const chHeight = expanded => (isTrue(expanded) ? 75 : 53);
 
-const drawChannelsGrid = (channels) => {
+const drawChannelsGrid = (timelineHeight) => {
   const c = document.getElementById('channelsTimeline');
   c.width = window.innerWidth - 256;
-  const canvasHeight =
-    channels
-      .map(ch => chHeight(ch.getIn(['payload', 'state', 'expanded'])))
-      .reduce(((a, b) => a + b), 0);
-  c.height = canvasHeight;
+  c.height = timelineHeight;
   const ctx = c.getContext('2d');
   ctx.strokeStyle = 'DimGray';
   for (let i = 0; i < 100; i++) {
     const x1 = (i * 40) + 0.5;
     ctx.moveTo(x1, 0);
-    ctx.lineTo(x1, canvasHeight);
+    ctx.lineTo(x1, timelineHeight);
     ctx.stroke();
     const x2 = (i * 40) + 20.5;
     ctx.moveTo(x2, 0);
-    ctx.lineTo(x2, canvasHeight);
+    ctx.lineTo(x2, timelineHeight);
     ctx.stroke();
   }
 };
@@ -34,14 +30,22 @@ class Timeline extends React.Component {
   constructor() {
     super();
     this.state = {
+      timelineHeight: 0,
       timelineWidth: 0,
     };
   }
 
   componentDidMount() {
     const setWidthDrawGrid = () => {
-      this.setState({ timelineWidth: window.innerWidth - 256 });
-      drawChannelsGrid(this.props.project.get('channels'));
+      const timelineHeight =
+        this.props.project.get('channels')
+          .map(ch => chHeight(ch.getIn(['payload', 'state', 'expanded'])))
+          .reduce(((a, b) => a + b), 0);
+      this.setState({
+        timelineHeight,
+        timelineWidth: window.innerWidth - 256,
+      });
+      drawChannelsGrid(timelineHeight);
     };
 
     window.addEventListener('resize', setWidthDrawGrid);
@@ -51,6 +55,8 @@ class Timeline extends React.Component {
   render() {
     const { i18n, moment, project, setMouseOut, setMouseOver, theme, updateProject,
       zoom } = this.props;
+    const { timelineHeight, timelineWidth } = this.state;
+
 
     const playbackX = findPlaybackPosition(moment, zoom);
     const timeStr = msToTimeString(moment);
@@ -58,7 +64,7 @@ class Timeline extends React.Component {
     return (
       <div className="overflow-x-hidden nowrap">
         <canvas id="channelsTimeline" className="absolute o-20" />
-        <div className="absolute" style={{ width: this.state.timelineWidth }}>
+        <div className="absolute" style={{ width: timelineWidth }}>
           {
             project.get('channels')
               .map(c => (
@@ -76,12 +82,16 @@ class Timeline extends React.Component {
               ))
           }
         </div>
-        <svg width={this.state.timelineWidth} height="225" className="absolute">
+        <svg
+          className="absolute"
+          height={timelineHeight}
+          width={timelineWidth}
+        >
           <line
             x1={playbackX}
             y1="0"
             x2={playbackX}
-            y2="130"
+            y2={timelineHeight}
             style={{ stroke: '#555555', strokeWidth: 1 }}
           />
         </svg>
